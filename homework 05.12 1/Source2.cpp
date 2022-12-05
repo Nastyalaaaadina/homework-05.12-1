aaf099d6b5c23d56b154196a047c8c34d2527c5b
Nastyalaaaadina <91201275+Nastyalaaaadina@users.noreply.github.com>
2022-12-05 18:37:15 +0300

    Add files via upload

---
+++ homework 05.12 1/Source2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <stdint.h>
+
+using namespace std;
+template <typename T>
+void swap(T a, T b) {// функци€, служаща€ дл€ обмена элементов a и 4b
+	int temp = *a;// temp Ц локальна€ переменна€, алгоритм циклического обмена
+	*a = *b;// значени€, наход€щиес€
+	*b = temp;// по данным адресам
+}
+template <typename T>
+T divide(T arr[], T min, T max) {
+	T pivot = arr[max]; // pivot как элемент, который разделит большую задачу на меньшую задачу.
+	T a = (min - 1);
+	for (T b = min; b <= max - 1; b++) {// цикл дл€ сравнени€ всех элементов с pivot 
+
+		if (arr[b] < pivot) {// дл€ эмента b, меньшего чем pivot 
+			a++;//после использовани€ переменной на данном шаге цикла или алгоритма, или в формуле значение переменной автоматически будет увеличено на 1
+			swap(&arr[a], &arr[b]);// обмен элементов
+		}
+	}
+	swap(&arr[a + 1], &arr[max]);
+	return (a + 1);// возврат значени€ а+1
+}
+template <typename T>
+void quickSort(T arr[], T min, T max)// функци€ дл€ сортировки
+{
+	if (min < max) {
+
+		int array_divide = divide(arr, min, max);// ввод разграничени€ массива
+		quickSort(arr, min, array_divide - 1);// быстра€ сортировка рекурсией элементов с левой стороны от элемента pivot
+		quickSort(arr, array_divide + 1, max);// быстра€ сортировка рекурсией элементов с правой стороны от элемента pivot
+	}
+}// print array function 
+template <typename T>
+void printArr(T arr[], T size)//функци€ printArr
+{
+	int i;
+	for (i = 0; i < size; i++) // при выполнении данного услови€...
+		cout << arr[i] << " ";// перебор всего массива и вывод элемента
+	cout << endl;
+}
+int main()// нагл€дный пример отсортированного массива
+{
+	int arr[] = { 12, 3, 5, 7, 8, 15, 20, 32, 6 };// мой массив
+
+	int size = sizeof(arr) / sizeof(arr[0]);// возвращает количество байтов, которое занимает массив
+
+	quickSort(arr, 0, size - 1);
+	cout << "Sorted array: \n";// вывод отсортированного массива с новой строки
+	printArr(arr, size); // методом, код которого указан ранее
+	
+     return 0;// возврат 0
+}
